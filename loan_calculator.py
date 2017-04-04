2e1edd7dcb2b4ede4151ec8a39c10a8933dfdff5
REDLION <REDLION>
2017-04-04 09:26:24 +0300

    added information

--- loan_calculator.py
+++ loan_calculator.py
@@ -1,7 +1,12 @@
 def loan_func(A, T, R):
-	rate = float(R)
-	time = float(T)
-	amount = float(A)
+
+	if R>0 and R<=100 and T>0 and T<=12 and A>0:
+		rate = R
+		time = T
+		amount = A
+
+		loan_payable = (amount + (amount * (rate/100) * (time/12)))
+
+		return loan_payable
 	
-	loan_payable = (amount + (amount * (rate/100) * (time/12)))
-	return loan_payable
+print (loan_func(100000,12,12))
--- loan_calculator_test.py
+++ loan_calculator_test.py
@@ -1,11 +1,35 @@
 import unittest
-from loan_calculator import 
+from loan_calculator import loan_func
 
-class loan_calculator(unittest.TestCase):
+class LoanCalculator(unittest.TestCase):
 	def test_it_works(self):
-		self assertEqual(loan_func(100000,12,12),112000, "error")
+		self.assertEqual(loan_func(100000,12,12), 112000, "error")
+	def test_lower_month(self):
+		self.assertEqual(loan_func(100000,-13,12), "Invalid month")
+		pass
+	def test_higher_month(self):
+		self.assertEqual(loan_func(100000,13,12), "Invalid month")
+		pass
+	def test_lower_rate_input(self):
+		self.assertEqual(loan_func(100000,12,-12), "Invalid Interest Rate")
+		pass
+	def test_higher_rate_input(self):
+		self.assertEqual(loan_func(100000,12,-12), "Invalid Interest Rate")
+		pass
+	def test_amount_value_exception(self):
+		self.assertRaises(ValueError, loan_func("ten", 12,12))
+		pass
+	def test_time_value_exception(self):
+		self.assertRaises(ValueError, loan_func(100000, "twelve",12))
+		pass
+	def test_rate_value_exception(self):
+		self.assertRaises(ValueError, loan_func(100000, 12,"twelve"))
+		pass
+	def test_amount_input(self):
+		self.assertEqual(loan_func(0,12,12), "Enter an amount please")
+		pass
 		
-if _name_ == "_main_":
+if __name__ == "__main__":
 	unittest.main()
 
 
