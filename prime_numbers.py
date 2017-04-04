f00ea85f3b13bea0a1b3fbec2d3bcdc7154f6117
REDLION <REDLION>
2017-04-04 13:42:48 +0300

    added information

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,10 +1,22 @@
 def prime_no (n):
-	if n>0:
-		for i in range(1,n+1):
-			for t in range(2,i):
+	a = list()
+	
+	if type(n) is not int or n == '':
+		return "invalid entry"
+
+	if n<0:
+		return "invalid input"
+
+	if n==0 or n==1:
+		return n," is not a prime number"
+
+	if n>1:#ensure the number is not a negative and it is greater than zero
+		for i in range(2,n+1):#get a list of all the numbers from 2 to my number
+			for t in range(2,i):#check if each number is a prime number in my list
 				if i%t==0:
 					break
-			else:
-				print(i)
+			else:#place the prime numbers in a list
+				a.append(i)
+	return a
 
-print (prime_no(15))
+print (prime_no(9))
---
+++ prime_test.py
@@ -0,0 +1,17 @@
+import unittest
+from prime_numbers import prime_no
+
+class LoanCalculator(unittest.TestCase):
+	def test_negative_input(self):
+		self.assertEqual(prime_no(-9), "invalid input")
+	def test_zero_input(self):
+		self.assertEqual(prime_no(0), "invalid input")
+	def test_working(self):
+		self.assertEqual(prime_no(9), [2, 3, 5, 7] , "Code Error")
+	def test_output_type(self):
+		self.assertEqual(type(prime_no(9))=='list', "type Error")
+	def test_input_type(self):
+		self.assertEqual(prime_no(20.6), "invalid input type")
+		
+if __name__ == "__main__":
+	unittest.main()
