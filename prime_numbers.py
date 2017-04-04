d06d25a281011ffd6555f134c9cf5a0a4573e4e3
Daniel Masake Kitui <dmkitui@gmail.com>
2017-04-04 12:50:02 +0300

    Proposed changes
    Comments on what needs to be done/redone at first glance.

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,10 +1,11 @@
 def prime_no (n):
-	if n>0:
+	if n>0: # Need to catch exception for negative numbers
 		for i in range(1,n+1):
-			for t in range(2,i):
+			for t in range(2,i): 
 				if i%t==0:
 					break
 			else:
-				print(i)
+				print(i) # Need to create an empty list to append this numbers that happen to be primes
+	#Use a return to return valie of the list of prime numbers detected
 
 print (prime_no(15))
