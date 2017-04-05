8cb942ef9d5fdc0c8c5910f6bed89938f7b547d7
REDLION <REDLION>
2017-04-05 12:58:20 +0300

    day three added

---
+++ word_count.py
@@ -0,0 +1,22 @@
+def words(line):
+	t=list()
+	if type(line)== str:#ensures that the input received is a string 
+		"""if (x=="\n" for x in line ):
+			break
+		else:
+			print (x)"""
+
+		t = line.split()#splits the string into a list of the words
+		
+		dic = {i:t.count(i) for i in t} #counts each word and keeps it in a dictionary with its count
+
+		print(dic)
+	else:
+		print("invalid input")
+
+
+words("sgn gfm")
+
+import sys
+
+print (sys.getdefaultencoding())
