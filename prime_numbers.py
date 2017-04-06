c8e3efdae270c9045c4e53eb61063f71665577e2
REDLION <REDLION>
2017-04-06 22:36:43 +0300

    Day four's work

--- prime_numbers.py
+++ prime_numbers.py
@@ -2,16 +2,16 @@ def prime_no (n):
 
 	a = list()
 	
-	if type(n) is not int or n == '':#checks the type of the value received
-		return "invalid entry"
+	if type(n) is not int or n == ' ':#checks the type of the value received
+		return "invalid input type"
 
-	if n<0:#ensure we get a value greater than 0
+		"""if n<0:#ensure we get a value greater than 0
+		return "invalid input"""
+				
+	elif n<=0 or n==1:#elliminate 1 and 0 from the list of prime numbers
 		return "invalid input"
 
-	if n==0 or n==1:#elliminate 1 and 0 from the list of prime numbers
-		return n," is not a prime number"
-
-	if n>1:#ensure the number is not a negative and it is greater than zero
+	elif n>1:#ensure the number is not a negative and it is greater than zero
 		for i in range(2,n+1):#get a list of all the numbers from 2 to my number
 			for t in range(2,i):#check if each number is a prime number in my list
 				if i%t==0:
